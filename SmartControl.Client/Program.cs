d93de63203db1d69fd1ba7287520871a1d4f94fd
babyperrito <babyperrito@gmail.com>
2025-10-14 09:43:38 +0200

    Aggiungere i file di progetto.

---
+++ SmartControl.Client/Program.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddDevExpressBlazor(options =>
+{
+    options.BootstrapVersion = DevExpress.Blazor.BootstrapVersion.v5;
+});
+
+await builder.Build().RunAsync();
